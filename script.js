e5c7cad3059083c9b6f3f343ec441638f91b621c
Arsh Ilahi <156427743+Arsh249@users.noreply.github.com>
2024-08-14 14:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const parseCode = (str) => {
   // your code here
+	 const parts = str.split('0').filter(part => part !== '');
+    
+    // Extract firstName, lastName, and id based on the filtered parts
+    const firstName = parts[0];
+    const lastName = parts[1];
+    const id = parts[2];
+    
+    // Return the object with extracted values
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        id: id
+    };
 };
 
 // Do not change the code below
